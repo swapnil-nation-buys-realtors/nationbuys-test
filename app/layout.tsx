6f6180d78eaa2190891ca615b5b13cd2c8bfca0f
troyboy95 <starparv9577@gmail.com>
2026-04-04 13:17:05 +0530

    og image added

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <meta property="og:image" content="/open-graph-image.jpeg" />
+        <meta property="og:title" content="Nation Buys Realtor  Prime Lands & Pre-Leased Yields" />
+        <meta property="og:description" content="Elevate Ambitions with NBR: Expert facilitation of prime land transactions and pre-leased commercial portfolios across Pune, Mumbai, Goa and Dubai." />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
